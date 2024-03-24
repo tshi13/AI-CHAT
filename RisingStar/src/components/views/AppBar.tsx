2c8b6035f3fe289fad03fbe16ca0ff0289085b96
Yulun <yulunliss001@gmail.com>
2024-03-24 14:42:31 -0400

    refactor routing, started profile page

--- RisingStar/src/components/views/AppBar.tsx
+++ RisingStar/src/components/views/AppBar.tsx
@@ -3,9 +3,9 @@ import { pages, user } from "../../data";
 import SwitchAccessShortcutRoundedIcon from "@mui/icons-material/SwitchAccessShortcutRounded";
 import { Avatar } from "@mui/material";
 
-function Header() {
+function AppBar() {
   return (
-    <div className="h-screen overflow-hidden">
+    <div className="overflow-hidden">
       <nav className="flex text-xl bg-black text-white sticky top-0 appbar items-center pl-4">
         <SwitchAccessShortcutRoundedIcon fontSize="large" />
         <Link to="home" className="hover-underline font-bold pl-2">
@@ -15,20 +15,22 @@ function Header() {
         <div className="absolute right-0 pr-8">
           <div className="flex gap-16 items-center">
             {pages.map((page) => (
-              <Link to={page.toLowerCase()} key={page} className="hover-underline">
+              <Link
+                to={page.toLowerCase()}
+                key={page}
+                className="hover-underline"
+              >
                 {page}
               </Link>
             ))}
-            <Avatar alt={user.name} src={user.avatar} />
+
+            <Link to={user.id} key={user.id}>
+              <Avatar alt={user.name} src={user.avatar} />
+            </Link>
           </div>
         </div>
-
-
       </nav>
-      <div className="content overflow-hidden">
-        <Outlet />
-      </div>
     </div>
   );
 }
-export default Header;
+export default AppBar;
--- RisingStar/src/main.tsx
+++ RisingStar/src/main.tsx
@@ -1,35 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./components/view/ErrorPage.tsx";
-import Home from "./components/view/Home.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ErrorPage from "./components/views/ErrorPage.tsx";
+import Home from "./components/pages/Home.tsx";
 import App from "./App.tsx";
-import Header from "./components/view/Header.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Header />,
-    errorElement: <ErrorPage/>,
-    children: [
-      {
-        path:"home",
-        element: <Home/>,
-        errorElement: <ErrorPage/>
-      },
-      {
-        path:"chat",
-        element: <App/>,
-        errorElement: <ErrorPage/>
-      },
-    ]
-  },
-  
-]);
+import AppBar from "./components/views/AppBar.tsx";
+import Profile from "./components/pages/Profile.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <AppBar />
+      <Routes>
+        <Route path="/" element={<Home/>} errorElement={<ErrorPage/>}/>
+        <Route path="/home" element={<Home/>} errorElement={<ErrorPage/>}/>
+        <Route path="/profile" element={<Profile/>} errorElement={<ErrorPage/>}/>
+        <Route path="/chat" element={<App/>} errorElement={<ErrorPage/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
