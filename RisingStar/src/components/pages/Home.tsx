91fb9120afd2a8a8726c6c3de616fb26e6fe9b25
Yulun <yulunliss001@gmail.com>
2024-03-27 00:04:19 -0400

    changed home to profile, remove profile page

--- RisingStar/src/components/pages/Home.tsx
+++ RisingStar/src/components/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
 import { user } from "@/data";
 import ProjectCard from "../ui/projectCard";
@@ -11,7 +10,7 @@ function Home() {
       <div className="col-span-2">
         <Button variant={"link"}>Continue where you left off &rarr;</Button>
         <div className="grid grid-cols-2 grid-rows-2 gap-2">
-          {user.projects.map((project) => (
+          {user.projects.slice(0,4).map((project) => (
             <ProjectCard project={project} height="h-28" width="w-28" fontSizeTitle="text-md" fontSizeRest="text-xs" lines="line-clamp-2"/>
           ))}
         </div>
--- RisingStar/src/main.tsx
+++ RisingStar/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ErrorPage from "./components/views/ErrorPage.tsx";
-import Home from "./components/pages/Home.tsx";
 import App from "./App.tsx";
 import AppBar from "./components/views/AppBar.tsx";
 import Profile from "./components/pages/Profile.tsx";
@@ -13,9 +12,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <AppBar />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:userId" element={<Profile />} />
+          <Route path="/" element={<Profile />} />
+          <Route path="/home" element={<Profile />} />
           <Route path="/chat" element={<App />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
